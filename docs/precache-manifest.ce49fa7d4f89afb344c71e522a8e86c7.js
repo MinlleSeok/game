23a088ff817327682f797c29acef960733d8c9ad
hotheating <>
2020-01-25 22:13:31 +0900

    hi

---
+++ docs/precache-manifest.ce49fa7d4f89afb344c71e522a8e86c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a89b7746ce98be29b5bbc6873f68c9",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "ef5c70fe8a364508bebf",
+    "url": "/game/static/css/main.0917bf81.chunk.css"
+  },
+  {
+    "revision": "9097124f24d5f278a832",
+    "url": "/game/static/js/2.7021b913.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/game/static/js/2.7021b913.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef5c70fe8a364508bebf",
+    "url": "/game/static/js/main.766bcce5.chunk.js"
+  },
+  {
+    "revision": "65fc70b869388428e8a9",
+    "url": "/game/static/js/runtime-main.ea3d9588.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c40c12b1adf743b1dfe759466820e1ca.js"
+  "/game/precache-manifest.ce49fa7d4f89afb344c71e522a8e86c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
