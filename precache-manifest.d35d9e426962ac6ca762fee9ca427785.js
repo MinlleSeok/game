8557c7d9e9592e8a9727bc1c93f1f26f9c3f8e3d
MinlleSeok <hotheating@gmail.com>
2020-02-04 18:36:37 +0900

    Updates

---
+++ precache-manifest.d35d9e426962ac6ca762fee9ca427785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a37dd4e43830e0a54e1aaec16f8264",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "9fb532edc6623c7731a7",
+    "url": "/game/static/css/main.3e29a838.chunk.css"
+  },
+  {
+    "revision": "7912b35026a74d94f932",
+    "url": "/game/static/js/2.94288c67.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/game/static/js/2.94288c67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fb532edc6623c7731a7",
+    "url": "/game/static/js/main.50c42d0c.chunk.js"
+  },
+  {
+    "revision": "65fc70b869388428e8a9",
+    "url": "/game/static/js/runtime-main.ea3d9588.js"
+  }
+]);
